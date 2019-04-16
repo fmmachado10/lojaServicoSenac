8355e55f00bc1e93e620669bc8b46da0fdd13a64
fmmachado10 <fabricio.machado@tcm.go.gov.br>
2019-04-16 18:10:46 -0300

    Adicionado classes Servico

---
+++ src/negocio/CadastroServicoCtrl.java
@@ -0,0 +1,141 @@
+package negocio;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import bean.Produto;
+import bean.Servico;
+import persistencia.ProdutoDAO;
+import persistencia.ServicoDAO;
+
+@ManagedBean
+@SessionScoped
+public class CadastroServicoCtrl {
+
+	private Servico servico;
+
+	private List<Servico> servicos; // = new ArrayList<>();
+
+	private Servico servicoSelecionado;
+
+	public Servico getServico() {
+
+		return servico;
+
+	}
+
+	public CadastroServicoCtrl() {
+
+		if (servico == null) {
+
+			this.servico = new Servico();
+
+		}
+
+		servicos = ServicoDAO.consultar();
+
+	}
+
+	public void setEquipamento(Servico servico) {
+
+		this.servico = servico;
+
+	}
+
+	public void actionGravar() {
+
+		if (servico.getId() == 0) {
+
+			ServicoDAO.inserir(servico);
+
+		} else {
+
+			ServicoDAO.alterar(servico);
+
+		}
+
+		limpar();
+
+	}
+
+	public void actionEditar() {
+
+		if (servico.getId() > 0) {
+
+			ServicoDAO.alterar(servico);
+
+		}
+
+		limpar();
+
+	}
+
+	public void actionExcluir() {
+
+		if (servico.getId() > 0) {
+
+			ServicoDAO.excluir(servico);
+
+		}
+
+		limpar();
+
+	}
+
+	
+	public List<Servico> getPesquisar() {
+
+		// return EquipamentoDao.pesquisar();
+
+		return null;
+
+	}
+
+	public void limpar() {
+
+		this.servico = new Servico();
+
+		this.servicos = ServicoDAO.consultar();
+
+		System.out.println("limpando...");
+
+	}
+
+	public void aoSelecionar() {
+
+		this.servico = servicoSelecionado;
+
+	}
+
+	public void aoDesselecionar() {
+
+		limpar();
+
+	}
+
+	public List<Servico> getServicos() {
+
+		return servicos;
+	}
+
+	public void setServicos(List<Servico> servicos) {
+
+		this.servicos = servicos;
+
+	}
+
+	public Servico getServicoSelecionado() {
+
+		return servicoSelecionado;
+
+	}
+
+	public void setServicoSelecionado(Servico servicoSelecionado) {
+
+		this.servicoSelecionado = servicoSelecionado;
+
+	}
+
+}
---
+++ src/persistencia/ServicoDAO.java
@@ -0,0 +1,29 @@
+package persistencia;
+
+import java.util.List;
+
+import bean.Servico;
+
+public class ServicoDAO {
+
+	public static List<Servico> consultar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void inserir(Servico servico) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void alterar(Servico servico) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void excluir(Servico servico) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
