353e82c3764c0f0f46fb327d8bfea893ba8de2cd
FABRÍCIO <FABRCIO@DESKTOP-600CU3V>
2019-04-25 13:50:54 -0300

    Spring Security

--- src/negocio/CadastroPessoaCtrl.java
+++ src/negocio/CadastroPessoaCtrl.java
@@ -3,6 +3,7 @@ package negocio;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 
 import bean.Pessoa;
